eb2608c8039cd7a5c5edc2d4cb1cf7cdd0adfaf7
dghhjejh <konaemmanuel0@gmail.com>
2024-06-07 03:35:51 -0400

    une todo list a été ajoutée... On peut ajouter des éléments dans l'array, les supprimer

--- src/components/CardGrids.vue
+++ src/components/CardGrids.vue
@@ -2,16 +2,58 @@
     <header>
         <NavBar/>
     </header>
+    <v-container class="leconteneur">
+        <v-row>
+           <v-col cols="12" md="8" class="mx-auto">
+            <v-text-field v-model="nouvelleTache" label="Ajouter une tâche" append-icon="mdi-plus" @click:append="ajouterTache">
+            </v-text-field>
+           </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" md="8" class="mx-auto">
+                <v-card
+                v-for="(tache, indice) in taches"
+                :key="indice"
+                class="mb-4"
+                >
+                <v-card-title>
+                    {{ tache }}
+                    <v-spacer></v-spacer>
+                    <v-btn icon @click="supprimerTache(indice)">
+                    <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                </v-card-title>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
+
 <script setup>
 import NavBar from './NavBar';
+import {ref} from 'vue';
+const nouvelleTache = ref('');
+const taches = ref([]);
+const ajouterTache = () =>{
+    if(nouvelleTache.value.trim() !== ''){
+        taches.value.push(nouvelleTache.value);
+        nouvelleTache.value = '';
+    }
+};
+
+const supprimerTache = (indice) =>{
+    taches.value.splice(indice, 1);
+};
 </script>
+
 <style>
 .leconteneur{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    width: 100vw;
+    position: fixed;
+    top: 110px;
+    z-index: 9999;
+}
+.mx-auto {
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
